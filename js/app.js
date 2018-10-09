4d36ef48e14bb759bbee564a5286a90ca537fc2d
Prateek Arora <prateekisthebest98@gmail.com>
2018-10-09 20:08:45 +0530

    Make use able

--- js/app.js
+++ js/app.js
@@ -1,36 +1,103 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// var Enemy = function() {
+//     // Variables applied to each of our instances go here,
+//     // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+//     // The image/sprite for our enemies, this uses
+//     // a helper we've provided to easily load images
+//     this.sprite = 'images/enemy-bug.png';
+// };
+    class Enemy{
+        constructor(x,y,speed){
+            this.x=x;
+            this.y=y;
+            this.speed=speed;
+            this.sprite='images/enemy-bug.png';
+        }
+        update(dt){
+          if (this.x < 505) {
+              this.x += this.speed*dt;
+          } else {
+            this.x = -101;
+          }
+        }
+        render(){
+            ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        }
+    }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+//Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+//};
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+//Enemy.prototype.render = function() {
+  //  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+//};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+class Player{
+    constructor(x,y,speed){
+        this.x=x;
+        this.y=y;
+        this.speed=speed;
+        this.sprite = 'images/char-boy.png';
+    }
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    handleInput(input){
+        switch (input) {
+            case 'left':
+                if(this.x > 0)
+                    this.x -= 101;
+                break;
+            case 'right':
+                if(this.x < 404)
+                    this.x += 101
+                break;
+            case 'up':
+                if(this.y > 0)
+                    this.y -= 83
+                break;
+            case 'down':
+                if(this.y < 387)
+                    this.y += 83
+                break;
+        }
+    }
+    update() {
+       for(let enemy of allEnemies) {//loops through every single enemy to get there indiviual x,y value
+            if (this.y === enemy.y && ( enemy.x + (138||55) / 2> this.x && enemy.x < this.x + 101/2)) {
+               alert('You loose');//this will show score and chances of player
+               this.reset();// it will going to reset the player to initial location
+           }
+           else if (this.y === -28) {//it checkes that user reached to the water
+               this.reset();//invokes reset method to place player back to initial position
+               }
+        }}
+        reset() {// these value sets player pos to starting point.
+            this.x = 202;//initial x pos of player
+            this.y = 387;//initial y pos of player
+        }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+const player = new Player(202,387);
+const bug1 = new Enemy(-101,55,300);
+const bug2 = new Enemy(-101,55+83,250);
+const bug3 = new Enemy((-101*2.5),55+83+83,400);
+const bug4 = new Enemy((-101*1.5),55+83,150);
+const allEnemies = [];
+allEnemies.push(bug1,bug2,bug3,bug4);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
